9b26d7de91dd44fbd5bda6aea563ce4e9b64e267
Harsh Patel <harshpatel5141@gmail.com>
2026-01-20 11:28:02 +0530

    Add files via upload

---
+++ pro1to9.py
@@ -0,0 +1,69 @@
+#pro 1
+
+#print ("harsh,22,valsad")
+
+#pro 2
+
+#a = 10
+
+#b = 20
+        
+#print(a+b)
+
+#pro 3
+
+#celsius = float(input("Enter temperature in Celsius: "))
+
+#fahrenheit = (celsius * 9/5) + 32
+
+#print("Temperature in Fahrenheit =", fahrenheit)
+
+#pro 4
+#name = "Harsh Patel"
+#print(name.upper())
+
+#pro 5
+#from datetime import date
+
+#birth_year = int(input("Enter your birth year: "))
+#current_year = date.today().year
+
+#age = current_year - birth_year
+
+#print("Your age is:", age)
+
+#pro 6
+#a = int(input("Enter first value: "))
+#b = int(input("Enter second value: "))
+
+#, b = b, a
+
+#print("After swapping:")
+#print("a =", a)
+#print("b =", b)
+
+#pro 7
+#length = float(input("Enter length of rectangle: "))
+#width = float(input("Enter width of rectangle: "))
+
+#area = length * width
+
+#print("Area of rectangle =", area)
+
+#pro 8
+#num = int(input("Enter a number: "))
+
+#if num > 0:
+#  print("The number is Positive")
+#elif num < 0:
+ #   print("The number is Negative")
+#else:
+ #   print("The number is Zero")
+ 
+#pro 9
+#num1 = float(input("Enter first number: "))
+#num2 = float(input("Enter second number: "))
+
+#average = (num1 + num2) / 2
+
+#print("Average =", average)
